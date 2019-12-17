d85627acac3949791bab54f9e8fddee09bdc47a9
YisalKhan <yisal1994@gmail.com>
2019-12-17 22:24:20 +0500

    update the api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // apiURL: 'local.camp-api'
-  apiURL: 'http://127.0.0.1:8000'
+  apiURL: 'https://murmuring-falls-17187.herokuapp.com/'
 };
 
 /*
