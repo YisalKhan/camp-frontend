eddb2547d20a8e43b8aede6080efd78c5546f315
Usama <muhammad.baig@linentech.net>
2020-01-09 23:11:31 +0500

    pubnub added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PubNubAngular } from 'pubnub-angular2';
 
 @Component({
     selector: 'app-root',
@@ -9,7 +10,8 @@ export class AppComponent implements OnInit {
   lat: any;
   lng: any;
 
-    constructor() {
+    constructor(pubnub: PubNubAngular) {
+        pubnub.init({ publishKey: 'pub-c-02414160-d913-45c5-8531-0eaa1dffa163', subscribeKey: 'sub-c-1901bc68-330b-11ea-a820-f6a3bb2caa12' });
     }
 
     ngOnInit() {
