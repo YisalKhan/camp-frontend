bfbc4c821914ab81e6932998090a8f5087abdb5a
YisalKhan <yisal1994@gmail.com>
2020-01-10 16:47:39 +0500

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,7 @@ export class AppComponent implements OnInit {
           // console.log(this.lat);
           localStorage.setItem('latitude', this.lat);
           localStorage.setItem('longitude', this.lng);
+          localStorage.setItem('asdf', 'asd');
         });
       }
     }
