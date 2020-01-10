a3bb9bfd7b6d241174b8dd45707780f096526ae3
YisalKhan <yisal1994@gmail.com>
2020-01-10 16:15:36 +0500

    removed pwa service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { PubNubAngular } from 'pubnub-angular2';
+// import { PubNubAngular } from 'pubnub-angular2';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,7 +20,7 @@ import { AuthGuard } from './shared';
         AppRoutingModule
     ],
     declarations: [AppComponent],
-    providers: [AuthGuard, PubNubAngular],
+    providers: [AuthGuard],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
