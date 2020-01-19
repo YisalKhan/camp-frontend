05966cfe09b609a4cbb09063b92b327dc93efa0a
YisalKhan <yisal1994@gmail.com>
2020-01-19 16:27:27 +0500

    make the https for staging api link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // apiURL: 'http://127.0.0.1:8000',
   // apiURL: 'https://murmuring-falls-17187.herokuapp.com/'
-  apiURL: 'http://api.themedicalcamp.com'
+  apiURL: 'https://api.themedicalcamp.com'
 };
