803ca9453e05b43875e6ff4bc04c59db10691795
YisalKhan <yisal1994@gmail.com>
2020-02-05 15:27:15 +0500

    create patient view

--- src/app/layout/patient/patient.module.ts
+++ src/app/layout/patient/patient.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PatientComponent } from './patient.component';
-// import { NgxSpinnerComponent } from 'ngx-spinner';
 import { PatientRoutingModule } from './patient-routing.module';
 import { ViewPatientComponent } from './view-patient/view-patient.component';
+import { PageHeaderModule } from 'src/app/shared';
 
+import { NgxSpinnerModule } from 'ngx-spinner';
 @NgModule({
   imports: [
     CommonModule,
     PatientRoutingModule,
     ReactiveFormsModule,
-    // NgxSpinnerComponent
+    PageHeaderModule,
+    NgxSpinnerModule
   ],
   declarations: [
     PatientComponent,
--- src/app/layout/patient/view-patient/view-patient.component.ts
+++ src/app/layout/patient/view-patient/view-patient.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { routerTransition } from '../../../router.animations';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+
 @Component({
   selector: 'app-view-patient',
   templateUrl: './view-patient.component.html',
-  styleUrls: ['./view-patient.component.scss']
+  styleUrls: ['./view-patient.component.scss'],
+  animations: [routerTransition()]
 })
 export class ViewPatientComponent implements OnInit {
 
-  constructor() { }
+  patients: any;
+
+  constructor(
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService
+  ) { }
 
   ngOnInit() {
+    this.spinner.show();
+    this.spinner.hide();
+  }
+
+  editPatient(pID) {
+    console.log('patient');
   }
 
 }
