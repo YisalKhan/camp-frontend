06efbdaa78fbe25cca43fb47bb89f31c8f0c2299
YisalKhan <yisal1994@gmail.com>
2020-02-06 23:04:12 +0500

    implemeted notifications

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { PubNubAngular } from 'pubnub-angular2';
 
 @Component({
     selector: 'app-header',
@@ -10,9 +11,15 @@ import { TranslateService } from '@ngx-translate/core';
 export class HeaderComponent implements OnInit {
     public pushRightClass: string;
     userData: any;
+    approvedCounter: any = 0;
+    createCounter: any = 0;
+    role: any;
 
-    constructor(private translate: TranslateService, public router: Router) {
-
+    constructor(
+        private translate: TranslateService,
+        private router: Router,
+        private pubnub: PubNubAngular
+    ) {
         this.router.events.subscribe(val => {
             if (
                 val instanceof NavigationEnd &&
@@ -22,11 +29,21 @@ export class HeaderComponent implements OnInit {
                 this.toggleSidebar();
             }
         });
+        this.pubnub.subscribe({ channels: ['createNotification', 'approvedNotification' ], triggerEvents: true, withPresence: true });
     }
 
     ngOnInit() {
+        this.role = localStorage.getItem('userDesignation');
         this.pushRightClass = 'push-right';
         this.userData = JSON.parse(localStorage.getItem('userData'));
+        this.pubnub.getMessage('createNotification', (msg) => {
+            console.log(msg);
+            this.createCounter =+ 1;
+        });
+        this.pubnub.getMessage('approvedNotification', (msg) => {
+            console.log(msg);
+            this.approvedCounter =+ 1;
+        });
     }
 
     isToggled(): boolean {
@@ -51,5 +68,15 @@ export class HeaderComponent implements OnInit {
 
     changeLang(language: string) {
         this.translate.use(language);
+    }
+
+    onApprovedNotification() {
+        this.approvedCounter = 0;
+        this.router.navigate(['/camps/approvedCamps']);
+    }
+
+    onCreateNotification() {
+        this.createCounter = 0;
+        this.router.navigate(['/camps/campsRequest']);
     }
 }
