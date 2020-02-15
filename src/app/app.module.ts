0c8364964cca274ff61a6ed4d0a9dbf162282609
YisalKhan <yisal1994@gmail.com>
2020-02-15 13:21:07 +0500

    fix issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { PubNubAngular } from 'pubnub-angular2';
-import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { ToastrModule } from 'ngx-toastr';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -24,7 +23,6 @@ import { TextMaskModule } from 'angular2-text-mask';
         LanguageTranslationModule,
         AppRoutingModule,
         TextMaskModule,
-        SweetAlert2Module.forRoot(),
         ToastrModule.forRoot(),
         ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
     ],
