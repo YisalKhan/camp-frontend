3d695799593c326579054f84f1200eb8b0b706aa
saif ali <saif.ali@live.se>
2020-02-16 21:56:05 +0500

    Icons added to cards in dashboard and camps

--- src/app/shared/modules/stat/stat.component.ts
+++ src/app/shared/modules/stat/stat.component.ts
@@ -8,11 +8,13 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class StatComponent implements OnInit {
     @Input() bgClass: string;
     @Input() icon: string;
+    @Input() iconImage: string;
     @Input() bgIcon: string;
     @Input() count: number;
     @Input() label: string;
     @Input() smallLabel: string;
     @Input() data: number;
+    @Input() customClass: number;
     @Output() event: EventEmitter<any> = new EventEmitter();
 
     constructor() {}
