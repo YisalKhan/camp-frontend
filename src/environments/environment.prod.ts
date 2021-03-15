cce5a145ae1d9e75c6f1083714f0372f86792add
Yisal Khan <yisal1994@gmail.com>
2021-03-15 16:19:17 +0500

    update the api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // apiURL: 'http://127.0.0.1:8000',
   // apiURL: 'https://murmuring-falls-17187.herokuapp.com/'
-  apiURL: 'https://api.themedicalcamp.com',
+  apiURL: 'https://demo.medical-camp.krapplet.com/public',
   pubnubChannel: 'channel_prod',
   pubnubCampLocaion: 'camp_location_prod'
 };
