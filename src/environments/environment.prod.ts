f4d6249beffd753d67b2d4caf28d408cdaba5642
yisalk <79074968+yisalk@users.noreply.github.com>
2022-01-15 13:25:30 +0500

    update API url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // apiURL: 'http://127.0.0.1:8000',
   // apiURL: 'https://murmuring-falls-17187.herokuapp.com/'
-  apiURL: 'https://demo.medical-camp.krapplet.com/public',
+  apiURL: 'https://demo.medical-camp.krapplet.com',
   pubnubChannel: 'channel_prod',
   pubnubCampLocaion: 'camp_location_prod'
 };
